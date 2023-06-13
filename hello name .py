8a8353f8d44a9d5def5a281bffd2aaa459c71293
mbuggs <136401416+mbuggs@users.noreply.github.com>
2023-06-12 18:23:29 -0700

    Add files via upload

---
+++ hello name .py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+myName= input('input your name')
+print (f" Hello{myName}")
+
+sum100=0
+
+for i in range(100):
+    sum100 = sum100 + (i+1)
+print(f' sum of first 100 integers is {sum100}')
